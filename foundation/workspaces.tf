e51877469585beda4f3ef998416f49b030139806
Marc Samendinger <git@samendinger.de>
2023-05-18 22:51:20 +0200

    Use set of strings as input

--- foundation/workspaces.tf
+++ foundation/workspaces.tf
@@ -17,7 +17,7 @@ resource "tfe_workspace" "workspace1" {
   description               = "workspace1"
   auto_apply                = true
   file_triggers_enabled     = true
-  remote_state_consumer_ids = tfe_workspace.workspace2.id
+  remote_state_consumer_ids = [tfe_workspace.workspace2.id]
   working_directory         = "/workspace1"
   vcs_repo {
     identifier                 = "msamendinger/tc-poc"
