44d0d4f5b99595b5e117f59a4b399a0ea50c2221
Marc Samendinger <git@samendinger.de>
2023-05-18 21:50:33 +0200

    Manage workspace1 and workspace2 in foundation

--- foundation/workspaces.tf
+++ foundation/workspaces.tf
@@ -1,12 +1,27 @@
-resource "tfe_workspace" "foundation" {
-  name                  = "foundation"
+resource "tfe_workspace" "workspace1" {
+  name                  = "workspace1"
   project_id            = tfe_project.POC.id
   organization          = "danielschniepp"
-  description           = "Terraform Cloud Configuration"
+  description           = "workspace1"
   auto_apply            = true
   file_triggers_enabled = true
-  working_directory     = "/foundation"
+  #remote_state_consumer_ids = tfe_workspace.workspace2.id
+  working_directory = "/workspace1"
   vcs_repo {
     identifier = "msamendinger/tc-poc"
   }
 }
+
+resource "tfe_workspace" "workspace2" {
+  name                  = "workspace2"
+  project_id            = tfe_project.POC.id
+  organization          = "danielschniepp"
+  description           = "workspace2"
+  auto_apply            = true
+  file_triggers_enabled = true
+  working_directory     = "/workspace2"
+  vcs_repo {
+    identifier = "msamendinger/tc-poc"
+  }
+}
+
